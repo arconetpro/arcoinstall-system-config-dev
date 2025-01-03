be924ac71bd1a0bb641b27d74692bdf6a277ea38
arcolinuxz <arcolinuxinfo@gmail.com>
2025-01-03 15:57:11 +0100

    update

--- up+.sh
+++ up+.sh
@@ -37,10 +37,7 @@ echo "Checking for newer files online first"
 git pull
 
 echo "getting latest empty pacman.conf from arconetpro"
-wget https://raw.githubusercontent.com/arcolinux/arcolinux-system-config/refs/heads/master/usr/local/share/arcolinux/pacman-no-arco.conf -O $workdir/usr/local/share/arcoinstall/pacman/pacman-no-arco.conf
-
-
-
+wget https://raw.githubusercontent.com/arcolinux/arcolinux-system-config/refs/heads/master/usr/local/share/arcolinux/pacman-no-arco.conf -O $workdir/usr/local/share/arcolinux/pacman-no-arco.conf
 
 # Below command will backup everything inside the project folder
 git add --all .
